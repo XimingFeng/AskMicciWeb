49cb63a336c381fb5a7406db83112b2a3ea2278b
XimingFeng <xfeng36@asu.edu>
2020-05-04 20:29:25 -0700

    start to put test for consumer

--- chat/views.py
+++ chat/views.py
@@ -6,8 +6,7 @@ from django.shortcuts import render
 def index(request):
     return render(request, "chat/index.html")
 
+def room(request):
+    return render(request, 'chat/room.html')
 
-def room(request, room_name):
-    return render(request, 'chat/room.html', {
-        'room_name': room_name
-    })
+
