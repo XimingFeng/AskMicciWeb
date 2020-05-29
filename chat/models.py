9f087e69800f99c6cdbb95464c75b48bcfb74b96
ximingfeng <xfeng36@asu.edu>
2020-05-28 21:56:53 -0700

    add field to the models

--- chat/models.py
+++ chat/models.py
@@ -8,12 +8,20 @@ import uuid
 class Room(models.Model):
     session_string = models.UUIDField(default=uuid.uuid4, editable=False)
     create_datetime = models.DateTimeField()
-    end_datetime = models.DateTimeField()
-    pass
+    end_datetime = models.DateTimeField(null=False)
 
+    def __str__(self):
+        return_str = "Room session: {} \n Created date time: {} \n End date time: {}"\
+            .format(self.session_string, self.create_datetime, self.end_datetime)
+        return return_str
 
 class ChatLine(models.Model):
     message_content = models.CharField(max_length=200)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
     create_datetime = models.DateTimeField()
+
+    def __str__(self):
+        return_str = "Message content: {} \n Room session: {} \n Created user: {} \n Created date time: {} "\
+            .format(self.message_content, self.room.session_string, self.user, self.create_datetime)
+        return return_str
